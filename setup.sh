5d2e039c79d6f27e8b3d8468027ba0e356918eb6
Patrick Alves <cpatrickalves@gmail.com>
2020-05-20 16:01:35 -0300

    Fix Heroku setup

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 mkdir -p ~/.streamlit/
-echo “\
+
+echo "\
 [general]\n\
-email = \”cpatrickalves@gmail.com\”\n\
-“ > ~/.streamlit/credentials.toml
-echo “\
+email = \"cpatrickalves@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-“ > ~/.streamlit/config.toml
+" > ~/.streamlit/config.toml
